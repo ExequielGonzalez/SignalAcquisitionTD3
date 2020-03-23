2f92b7e2799bce494c33478e3a473b99b36c4478
GonzalezExequiel <gonzalez-exequiel@hotmail.com>
2020-03-22 21:15:57 -0300

    ahora python envia la informacion a arduino, y el arduino responde

--- arduinoEnviarSerie/arduinoEnviarSerie.ino
+++ arduinoEnviarSerie/arduinoEnviarSerie.ino
@@ -1,16 +1,70 @@
 int i = 0;
+const int pinLED = 13;
 
 void setup()
 {
-    Serial.begin(9600);
+   Serial.begin(9600);
+   pinMode(pinLED, OUTPUT);
+   digitalWrite(pinLED, LOW);
 }
 
 void loop()
 {
-    for (i = 0; i < 255; i++)
-    {
-        Serial.println(i);
-        delay(10);
-    }
-    i = 0;
+   if (Serial.available() > 0)
+   {
+      char option = Serial.read();
+      if (option == '9')
+      {
+         digitalWrite(pinLED, HIGH);
+         Serial.print('5');
+         for (int i = 0; i < 3; i++)
+         {
+            digitalWrite(pinLED, HIGH);
+            delay(200);
+            digitalWrite(pinLED, LOW);
+            delay(200);
+         }
+         do
+         {
+            Serial.println(i);
+            delay(10);
+
+            i++;
+         } while (Serial.read() != '8');
+      }
+      for (int i = 0; i < 3; i++)
+      {
+         digitalWrite(pinLED, HIGH);
+         delay(200);
+         digitalWrite(pinLED, LOW);
+         delay(200);
+      }
+   }
 }
+
+// const int pinLED = 13;
+
+// void setup()
+// {
+//    Serial.begin(9600);
+//    pinMode(pinLED, OUTPUT);
+// }
+
+// void loop()
+// {
+//    if (Serial.available()>0)
+//    {
+//       char option = Serial.read();
+//       if (option >= '1' && option <= '9')
+//       {
+//          option -= '0';
+//          for (int i = 0;i<option;i++)
+//          {
+//             digitalWrite(pinLED, HIGH);
+//             delay(100);
+//             digitalWrite(pinLED, LOW);
+//             delay(200);
+//          }
+//       }
+//    }
+// }
--- leerSerie.py
+++ leerSerie.py
@@ -12,28 +12,45 @@ archivo_texto = open("pruebas.csv", "w")  # apertura en modo escritura
 archivo_temp = []
 tiempo = 0
 arduino = serial.Serial('COM3', 9600)  # se abre el puerto serie
-time.sleep(1)
+time.sleep(2)
+arduino.write(b'9')  # empieza la transimision de arduino
+try:  # para chequear si el arduino responde los mensajes
+    lectura = arduino.readline().decode('utf-8')
+    lectura = lectura[0:-2]
+except:
+    lectura = 6
+if lectura != 6:
+    print(lectura)
+    time.sleep(0.6)
+    for i in range(100):
+        try:
+            rawString = arduino.readline().decode('utf-8')
+        except UnicodeDecodeError:  # Si no se puede decodificar el primer dato por un error de sincronización se saltea
+            continue
 
-while True:
-    try:
-        rawString = arduino.readline().decode('utf-8')
-    except UnicodeDecodeError:  # Si no se puede decodificar el primer dato por un error de sincronización se saltea
-        continue
-
-    rawString = rawString[0:-2]
-    print(rawString)  # se elimina el /r/n
-    # el dato byte se convierte a string
-    archivo_temp.append([tiempo, int(rawString)])
-    tiempo += 0.000392
-    # se guarda en un txt
-    if keyboard.is_pressed('s'):  # se apreta 's' para salir
-        break
-
-with archivo_texto:  # se escriben los datos leidos en el archivo .csv
-    writer = csv.writer(archivo_texto)
-    writer.writerow(['Tiempo', 'Valor'])
-    for row in archivo_temp:
-        writer.writerow(row)
+        rawString = rawString[0:-2]
+        print(rawString)  # se elimina el /r/n
+        # el dato byte se convierte a string
+        archivo_temp.append([tiempo, int(rawString)])
+        tiempo += 0.000392
+        # se guarda en un txt
+        if keyboard.is_pressed('s'):  # se apreta 's' para salir
+            break
+    arduino.write(b'8')  # termina la transmision de arduino
+    with archivo_texto:  # se escriben los datos leidos en el archivo .csv
+        writer = csv.writer(archivo_texto)
+        writer.writerow(['Tiempo', 'Valor'])
+        for row in archivo_temp:
+            writer.writerow(row)
+else:
+    print('puerto serie no disponible')
 archivo_texto.close()  # se cierra el archivo
 
 arduino.close()  # se cierra el puerto serie
+
+# import serial
+# import time
+# arduino = serial.Serial("COM3", 9600)
+# time.sleep(2)
+# arduino.write(b'9')
+# arduino.close()
